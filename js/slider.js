4faa8e8ac1a5b2c19f8189f2f40917b3c25b546a
exploit-j <kkarismajh@gmail.com>
2022-06-15 17:43:41 +0900

    carousel 설정

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+const smSlide = tns({
+  container: ".slide-sm",
+  navContainer: ".slide .slide-nav",
+  controlsContainer: ".slide .slide-ctrl",
+  autoplay: true,
+  autoplayTimeout: 4000,
+});
+
+const lgSlide = tns({
+  container: ".slide-lg",
+  navContainer: ".slide .slide-nav",
+  controlsContainer: ".slide .slide-ctrl",
+  autoplay: true,
+  autoplayTimeout: 4000,
+});
