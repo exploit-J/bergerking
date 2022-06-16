344f14eb3b87fe9c83c934f0352a62c35d128b45
exploit-j <kkarismajh@gmail.com>
2022-06-16 16:45:22 +0900

    style 수정

--- js/gnb.js
+++ js/gnb.js
@@ -1,9 +1,19 @@
 const gnbMenu = document.querySelector("header .menu");
 const gnbArea = document.querySelector(".menu-area");
-console.log(gnbMenu);
+const navOpenButton = document.querySelector("header .nav-open");
+const navCloseButton = document.querySelector("header .nav-close");
+const navMenuItem = document.querySelectorAll("header .menu-item");
+console.log(navMenuItem);
+
 gnbMenu.addEventListener("mouseover", () => {
   gnbArea.classList.add("active");
 });
 gnbMenu.addEventListener("mouseout", () => {
   gnbArea.classList.remove("active");
+});
+navOpenButton.addEventListener("click", () => {
+  document.querySelector("nav").classList.add("active");
+});
+navCloseButton.addEventListener("click", () => {
+  document.querySelector("nav").classList.remove("active");
 });
